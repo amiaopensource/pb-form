055d02e0bc732da7fc375628f3150ff9b0dddd1b
Mark Bussey <mark@curationexperts.com>
2014-10-08 17:45:05 -0400

    Require nokogiri

--- app/models/record.rb
+++ app/models/record.rb
@@ -1,24 +1,9 @@
+require 'nokogiri'
 class Record < ActiveRecord::Base
-
-  # attr_accessible available_fields
-
-  def terms_for_input
-     [
-        :program_title,
-        :series_title,
-        :supplied_title,
-        :alternative_title,
-        :episode_title,
-        :clip_title,
-        :asset_type,
-        :unique_id,
-        :unique_id_source
-     ]
-  end
 
   def to_pbcore_xml
     xml = '<?xml version="1.0"?><pbcoreDescriptionDocument xmlns="http://www.pbcore.org/PBCore/PBCoreNamespace.html" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns="http://www.pbcore.org/PBCore/PBCoreNamespace.html" xsi:schemaLocation="http://www.pbcore.org/PBCore/PBCoreNamespace.html"></pbcoreDescriptionDocument>'
-    Nokogiri::XML(xml)
+    ::Nokogiri::XML(xml)
   end
 
 end
